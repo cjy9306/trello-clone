e9f869db25dd4237088eb1f7f9f1ef36a7886094
JAEYONG CHOI <cjy@JAEYONGui-MacBookPro.local>
2020-03-03 22:49:36 +0900

    code refactoring

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -1,7 +1,7 @@
 import client from './client';
 
-export const socialLogin = async ({data}) => client.post('/api/auth/socialLogin', data);
-export const login = async ({username, password}) => client.post('/api/auth/login', {username, password});
-export const register = async ({data}) => client.post('/api/auth/register', data);
-export const check = async ({username}) => client.post('/api/auth/check', {username});
-export const verify = async ({username, verify_code}) => client.post('/api/auth/verify', {username, verify_code});
+export const socialLogin = async ({data}) => client.post('/auth/socialLogin', data);
+export const login = async ({username, password}) => client.post('/auth/login', {username, password});
+export const register = async ({data}) => client.post('/auth/register', data);
+export const check = async ({username}) => client.post('/auth/check', {username});
+export const verify = async ({username, verify_code}) => client.post('/auth/verify', {username, verify_code});
--- src/lib/api/board.js
+++ src/lib/api/board.js
@@ -1,35 +1,35 @@
 import client from './client';
 
-export const createBoard = ({data}) => client.post('/api/board', data);
-export const getBoard = ({boardId}) => client.get('/api/board/' + boardId);
-export const deleteBoard = ({boardId}) => client.delete('/api/board/' + boardId);
-export const updateCardSeq = ({boardId, list_id, data}) => client.put('/api/board/' + boardId  + '/list/' + list_id + '/card/seq', data); 
-export const updateListSeq = ({boardId, data}) => client.put('/api/board/' + boardId + '/list/seq', data);
+export const createBoard = ({data}) => client.post('/board', data);
+export const getBoard = ({boardId}) => client.get('/board/' + boardId);
+export const deleteBoard = ({boardId}) => client.delete('/board/' + boardId);
+export const updateCardSeq = ({boardId, list_id, data}) => client.put('/board/' + boardId  + '/list/' + list_id + '/card/seq', data); 
+export const updateListSeq = ({boardId, data}) => client.put('/board/' + boardId + '/list/seq', data);
 
 // list
-export const createList = ({boardId, data}) => client.post('/api/board/' + boardId + '/list', data);
+export const createList = ({boardId, data}) => client.post('/board/' + boardId + '/list', data);
 
 // card
-export const createCard = ({boardId, list_id, data}) => client.post('/api/board/' + boardId + '/list/' + list_id + '/card', data);
-export const getCard = ({boardId, card_id}) => client.get('/api/board/' + boardId + '/card/' + card_id);
-export const updateCardDescription = ({boardId, card_id, data}) => client.put('/api/board/' + boardId + '/card/' + card_id + '/description', data);
-export const updateCardDueDate = ({boardId, card_id, data}) => client.put('/api/board/' + boardId + '/card/' + card_id + '/due_date', data);
+export const createCard = ({boardId, list_id, data}) => client.post('/board/' + boardId + '/list/' + list_id + '/card', data);
+export const getCard = ({boardId, card_id}) => client.get('/board/' + boardId + '/card/' + card_id);
+export const updateCardDescription = ({boardId, card_id, data}) => client.put('/board/' + boardId + '/card/' + card_id + '/description', data);
+export const updateCardDueDate = ({boardId, card_id, data}) => client.put('/board/' + boardId + '/card/' + card_id + '/due_date', data);
 
 // checklist
-export const createCheckList = ({boardId, card_id, data}) => client.post('/api/board/' + boardId + '/card/' + card_id + '/checklist', data);
-export const deleteCheckList = ({boardId, card_id, checklist_id}) => client.delete('/api/board/' + boardId + '/card/' + card_id + '/checklist/' + checklist_id);
-export const getCheckList = ({boardId, card_id}) => client.get('/api/board/' + boardId + '/card/' + card_id + '/checklist');
+export const createCheckList = ({boardId, card_id, data}) => client.post('/board/' + boardId + '/card/' + card_id + '/checklist', data);
+export const deleteCheckList = ({boardId, card_id, checklist_id}) => client.delete('/board/' + boardId + '/card/' + card_id + '/checklist/' + checklist_id);
+export const getCheckList = ({boardId, card_id}) => client.get('/board/' + boardId + '/card/' + card_id + '/checklist');
 
 // checklist item
-export const createCheckListItem = ({boardId, checklist_id, data}) => client.post('/api/board/' + boardId + '/checklist/' + checklist_id, data);
-export const updateCheckListItem = ({boardId, item_id, data}) => client.put('/api/board/' + boardId + '/checklist_item/' + item_id, data);
-export const deleteCheckListItem = ({boardId, item_id}) => client.delete('/api/board/' + boardId + '/checklist_item/' + item_id);
+export const createCheckListItem = ({boardId, checklist_id, data}) => client.post('/board/' + boardId + '/checklist/' + checklist_id, data);
+export const updateCheckListItem = ({boardId, item_id, data}) => client.put('/board/' + boardId + '/checklist_item/' + item_id, data);
+export const deleteCheckListItem = ({boardId, item_id}) => client.delete('/board/' + boardId + '/checklist_item/' + item_id);
 
 // comment    
-export const createComment = ({boardId, card_id, data}) => client.post('/api/board/' + boardId + '/card/' + card_id + '/comment', data);
-export const deleteComment = ({boardId, comment_id}) => client.delete('/api/board/' + boardId + '/comment/' + comment_id);
-export const getAllLabels = ({boardId}) => client.get('/api/board/' + boardId + '/labels');
-export const updateCardLabel = ({boardId, card_id, label_id, data}) => client.put('/api/board/' + boardId + '/card/' + card_id + '/labels/' + label_id, data);
-export const getBoardMembers = ({boardId}) => client.get('/api/board/' + boardId + '/members');
-export const addBoardMember = ({boardId, data}) => client.post('/api/board/' + boardId + '/members', data);
-export const deleteBoardMember = ({boardId, member_id}) => client.delete('/api/board/' + boardId + '/members/' + member_id);
+export const createComment = ({boardId, card_id, data}) => client.post('/board/' + boardId + '/card/' + card_id + '/comment', data);
+export const deleteComment = ({boardId, comment_id}) => client.delete('/board/' + boardId + '/comment/' + comment_id);
+export const getAllLabels = ({boardId}) => client.get('/board/' + boardId + '/labels');
+export const updateCardLabel = ({boardId, card_id, label_id, data}) => client.put('/board/' + boardId + '/card/' + card_id + '/labels/' + label_id, data);
+export const getBoardMembers = ({boardId}) => client.get('/board/' + boardId + '/members');
+export const addBoardMember = ({boardId, data}) => client.post('/board/' + boardId + '/members', data);
+export const deleteBoardMember = ({boardId, member_id}) => client.delete('/board/' + boardId + '/members/' + member_id);
