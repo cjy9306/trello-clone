97478d7b10098e7dbad620a08c9237cde6c0158f
cjy9306 <richson1@naver.com>
2020-03-03 22:49:36 +0900

    code refactoring

--- src/lib/api/member.js
+++ src/lib/api/member.js
@@ -1,5 +1,5 @@
 import client from './client';
 
-export const getAllBoards = ({member_id}) => client.get('/api/member/' + member_id + '/boards');
-export const getTeams = async ({memberId}) => client.get('/api/member/' + memberId + '/teams');
+export const getAllBoards = ({member_id}) => client.get('/member/' + member_id + '/boards');
+export const getTeams = async ({memberId}) => client.get('/member/' + memberId + '/teams');
 
--- src/lib/api/team.js
+++ src/lib/api/team.js
@@ -1,7 +1,7 @@
 import client from './client';
 
-export const createTeam = async ({data}) => client.post('/api/team', data);
-export const getTeam = ({teamId}) => client.get('/api/team/' + teamId);
-export const deleteTeam = ({teamId}) => client.delete('/api/team/' + teamId);
-export const addTeamMember = ({teamId, data}) => client.post('/api/team/' + teamId + '/member', data);
-export const deleteTeamMember = ({teamId, memberId}) => client.delete('/api/team/' + teamId + '/member/' + memberId);
+export const createTeam = async ({data}) => client.post('/team', data);
+export const getTeam = ({teamId}) => client.get('/team/' + teamId);
+export const deleteTeam = ({teamId}) => client.delete('/team/' + teamId);
+export const addTeamMember = ({teamId, data}) => client.post('/team/' + teamId + '/member', data);
+export const deleteTeamMember = ({teamId, memberId}) => client.delete('/team/' + teamId + '/member/' + memberId);
