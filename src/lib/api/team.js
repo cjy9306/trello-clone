bc58d0d1bc212d4bb55d4303d681ee9d2e88da20
JAEYONG CHOI <cjy@JAEYONGui-MacBookPro.local>
2020-03-03 23:33:57 +0900

    add feature: update team description

--- src/lib/api/team.js
+++ src/lib/api/team.js
@@ -2,6 +2,8 @@ import client from './client';
 
 export const createTeam = async ({data}) => client.post('/team', data);
 export const getTeam = ({teamId}) => client.get('/team/' + teamId);
+export const updateTeam = ({teamId, data}) => client.put('/team/' + teamId, data);
 export const deleteTeam = ({teamId}) => client.delete('/team/' + teamId);
+
 export const addTeamMember = ({teamId, data}) => client.post('/team/' + teamId + '/member', data);
 export const deleteTeamMember = ({teamId, memberId}) => client.delete('/team/' + teamId + '/member/' + memberId);
