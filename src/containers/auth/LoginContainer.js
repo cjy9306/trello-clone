8774dc7305f09098c9112783c60f5d7a337a1db5
JAEYONG CHOI <cjy@JAEYONGui-MacBookPro.local>
2020-03-04 21:40:20 +0900

    modify feature: set token client single tone header, instead of setting middleware of axios

--- src/containers/auth/LoginContainer.js
+++ src/containers/auth/LoginContainer.js
@@ -1,38 +1,38 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 import LoginContent from './LoginContent';
 import { useDispatch } from 'react-redux';
 import { login } from '../../modules/auth';
+import client from '../../lib/api/client';
 
 const LoginContainer = () => {
+	const dispatch = useDispatch();
+	const history = useHistory();
 
-    const dispatch = useDispatch();
-    const history = useHistory();
-    
-    const [alertVisible, setAlertVisible] = useState('');
+	const [alertVisible, setAlertVisible] = useState('');
 
-    const onLogin = async ({username, password}) => {
-        const result = await dispatch(login({username, password}));
+	const onLogin = async ({ username, password }) => {
+		const result = await dispatch(login({ username, password }));
 
-        if (result && result.success) {
-            const token = result.data.data.token;
-            const memberId = result.data.data.member_id;
-            sessionStorage.setItem('token', token);
-            sessionStorage.setItem('memberId', memberId);
-            sessionStorage.setItem('username', username);
+		if (result && result.success) {
+			const token = result.data.data.token;
+			const memberId = result.data.data.member_id;
+			sessionStorage.setItem('token', token);
+			sessionStorage.setItem('memberId', memberId);
+			sessionStorage.setItem('username', username);
 
-            history.push('/member/' + username + '/boards');
-            return true;
-        } else {
-            setAlertVisible(true);
-        }
+			client.defaults.headers.AccessToken = token;
 
-        return false;
-    };
+			history.push('/member/' + username + '/boards');
+			return true;
+		} else {
+			setAlertVisible(true);
+		}
 
-    return (
-        <LoginContent onLogin={onLogin} alertVisible={alertVisible}/>
-    )
+		return false;
+	};
+
+	return <LoginContent onLogin={onLogin} alertVisible={alertVisible} />;
 };
 
-export default React.memo(LoginContainer)
+export default React.memo(LoginContainer);
