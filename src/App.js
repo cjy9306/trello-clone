127ed4a16ddf681bba70d16d11265e343cac7691
cjy9306 <richson1@naver.com>
2020-03-10 20:58:28 +0900

    add feature: apply dotenv

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import BoardConatiner from './containers/board/BoardContainer';
 import LoginContainer from './containers/auth/LoginContainer';
 import MemberBoardsContainer from './containers/member/MemberBoardsContainer';
 import TeamSettingsContainer from './containers/Team/TeamSettingsContainer';
-import './main.css'
+import './main.css';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 function App() {
-    return (
-        <>
-            <Route exact component={LoginContainer} path='/' />
-            <Route exact component={MemberBoardsContainer} path='/member/:memberId/boards' />
-            <Route exact component={TeamSettingsContainer} path='/team/:teamId/settings' />
-            <Route exact component={BoardConatiner} path='/board/:boardId' />
-        </>
-    );
+	return (
+		<>
+			<Route exact component={LoginContainer} path="/" />
+			<Route exact component={MemberBoardsContainer} path="/member/:memberId/boards" />
+			<Route exact component={TeamSettingsContainer} path="/team/:teamId/settings" />
+			<Route exact component={BoardConatiner} path="/board/:boardId" />
+		</>
+	);
 }
 
 export default App;
