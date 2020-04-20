d1b8af6ba7688344a670c7885d691b678622e2e6
JAEYONG CHOI <richson1@naver.com>
2020-04-20 20:13:32 +0900

    apply eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": ["airbnb", "plugin:react-hooks/recommended", "eslint-config-prettier"],
+    "plugins": [
+        "react", "prettier"
+	],
+	rules: {
+		"prettier/prettier": "error",
+	}
+};
